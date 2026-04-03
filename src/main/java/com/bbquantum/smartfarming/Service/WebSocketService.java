fdac572255955ac7c0fc88cb511a2d9c4860a6c3
Benney-Walker <benjaminbadubenney@gmail.com>
2026-04-03 12:26:22 +0000

    Updated web socket service

--- src/main/java/com/bbquantum/smartfarming/Service/WebSocketService.java
+++ src/main/java/com/bbquantum/smartfarming/Service/WebSocketService.java
@@ -1,6 +1,8 @@
 package com.bbquantum.smartfarming.Service;
 
+import com.bbquantum.smartfarming.DTO.WebSocketDTO.ActivityDTO;
 import com.bbquantum.smartfarming.DTO.WebSocketDTO.AlertData;
+import com.bbquantum.smartfarming.DTO.WebSocketDTO.RecentIrrigationCommands;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,18 @@ public class WebSocketService {
     public void sendAlerts(AlertData alertData) {
         messagingTemplate.convertAndSend(
                 "/topic/alerts", alertData
+        );
+    }
+
+    public void sendActivities(ActivityDTO activityDTO) {
+        messagingTemplate.convertAndSend(
+                "/topic/activities", activityDTO
+        );
+    }
+
+    public void sendRecentIrrigation(RecentIrrigationCommands recentIrrigationCommands) {
+        messagingTemplate.convertAndSend(
+                "/topic/recent_irrigation", recentIrrigationCommands
         );
     }
 }
